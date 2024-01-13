0b0b0dcc7cdfcb746256badd330354837b054fad
JK-mern <Jkmerndev@gmail.com>
2024-01-13 23:18:00 +0530

    completed navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,19 +2,26 @@ import Home from "./Pages/Home";
 import Profile from "./Pages/Profile";
 import SignIn from "./Pages/SignIn";
 import SignUp from "./Pages/SignUp";
+import Header from "./Components/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Problems from "./Pages/Problems";
+import About from "./Pages/About";
 
 function App() {
   return (
-    <BrowserRouter >
-    <Routes>
-      <Route path="/" element= {<Home />} />
-      <Route path="/sign-in" element = {<SignIn />} />
-      <Route path= '/sign-up' element = {<SignUp />} />
-      <Route path="/profile" element = {<Profile />} />
-    </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/problems" element={<Problems />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,7 +1,33 @@
+import { Link } from "react-router-dom";
 function Header() {
-  return <div>
-    <h1>Header of Website</h1>
-  </div>;
+  return (
+    <div className="flex  p-5 justify-between items-center cursor-pointer md:mx-20  ">
+      <Link to='/'>
+        <div className="flex gap-1 ">
+          <h1 className="font-bold text-3xl">Algo</h1>
+          <span className="text-3xl font-semibold text-cyan-700">Xpert</span>
+        </div>
+      </Link>
+      <div>
+        <ul className="flex items-center gap-4 text-base ">
+          <Link to="/">
+            <li className="hidden md:inline  hover:opacity-75">Home</li>{" "}
+          </Link>
+          <Link to="/problems">
+            <li className="hidden md:inline hover:opacity-75">Problems</li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden md:inline hover:opacity-75">About</li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="text-xl md:text-base rounded-md  hover:opacity-75">
+              SignIn
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
