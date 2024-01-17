7c554480dece79fff235c8cfc43ed33079c16d7a
JK-mern <Jkmerndev@gmail.com>
2024-01-17 23:27:05 +0530

    created a error handling middleware

--- server/index.js
+++ server/index.js
@@ -17,9 +17,11 @@ app.use("/api/auth",authRouter)
 
 
 
-
-app.get('/' , (req,res) => {
-    res.send("Hello world")
+app.use((err,req,res,next) =>{
+    console.log(err)
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({ success: false, statusCode, message });
 })
 
 app.listen( port , () =>{
