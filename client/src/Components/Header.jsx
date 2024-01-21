f2730fd19ed1b5c7b0989c79ad8941e4a27ab3fb
JK-mern <Jkmerndev@gmail.com>
2024-01-21 22:42:24 +0530

    updated header component to show user profile once signed in

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,8 +1,10 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="flex  p-5 justify-between items-center cursor-pointer md:mx-20  ">
-      <Link to='/'>
+      <Link to="/">
         <div className="flex gap-1 ">
           <h1 className="font-bold text-3xl">Algo</h1>
           <span className="text-3xl font-semibold text-cyan-700">Xpert</span>
@@ -19,10 +21,18 @@ function Header() {
           <Link to="/about">
             <li className="hidden md:inline hover:opacity-75">About</li>
           </Link>
-          <Link to="/sign-in">
-            <li className="text-xl md:text-base rounded-md  hover:opacity-75">
-              SignIn
-            </li>
+
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                src={currentUser.avatar}
+                className="w-10 h-10 rounded-full object-cover"
+              />
+            ) : (
+              <li className="text-xl md:text-base rounded-md  hover:opacity-75">
+                SignIn
+              </li>
+            )}
           </Link>
         </ul>
       </div>
