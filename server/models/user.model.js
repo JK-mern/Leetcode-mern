8d73763e23646154347bce496144195739ad6200
JK-mern <Jkmerndev@gmail.com>
2024-01-22 18:22:01 +0530

    updated user model for adding admin authorization

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,28 +1,34 @@
 import mongoose from "mongoose";
 
-const userScheme = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const userScheme = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  avatar: {
-    type: String,
-    default:
-      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
-  },
-  password: {
-    type : String,
-    required : true
-  }
-}, {timestamps : true});
+  { timestamps: true }
+);
 
-const User = new mongoose.model( 'User' , userScheme)
-export default User
-
-
+const User = new mongoose.model("User", userScheme);
+export default User;
