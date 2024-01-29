1516427c968eb2dd322d401be0b6ad57e3f24154
JK-mern <Jkmerndev@gmail.com>
2024-01-29 23:53:02 +0530

    made the class dynamically to render text color based on the difficulty of question

--- client/src/Pages/Problems.jsx
+++ client/src/Pages/Problems.jsx
@@ -21,8 +21,10 @@ function Problems() {
     setSkipValue(skipValue + 10);
   };
   const handlePrev = () => {
-    setSkipValue(Math.max(0, skipValue-10))
+    setSkipValue(Math.max(0, skipValue - 10));
   };
+
+  console.log(questions)
 
   return (
     <main className="max-w-6xl mx-auto my-10">
@@ -40,18 +42,38 @@ function Problems() {
             {questions.length >= 1 &&
               questions.map((question, index) => (
                 <tr key={index}>
-                  <th>{skipValue+index + 1}</th>
+                  <th>{skipValue + index + 1}</th>
                   <td className="cursor-pointer">{question.title}</td>
-                  <td>{question.difficulty}</td>
+                  <td
+                    className={
+                      question.difficulty === "Easy"
+                        ? "text-green-600"
+                        : question.difficulty === "Medium"
+                        ? "text-amber-500"
+                        : question.difficulty === "Hard"
+                        ? "text-red-500"
+                        : ""
+                    }
+                  >
+                    {question.difficulty}
+                  </td>
                 </tr>
               ))}
           </tbody>
         </table>
         <div className="flex flex-row my-6 justify-center gap-4  ">
-          <button className="btn btn-active btn-ghost" disabled={skipValue ===0} onClick={handlePrev}>
+          <button
+            className="btn btn-active btn-ghost"
+            disabled={skipValue === 0}
+            onClick={handlePrev}
+          >
             prev
           </button>
-          <button className="btn btn-active btn-ghost " disabled= {questions.length <10} onClick={handleNext}>
+          <button
+            className="btn btn-active btn-ghost "
+            disabled={questions.length < 10}
+            onClick={handleNext}
+          >
             next
           </button>
         </div>
