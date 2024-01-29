e6b9e0c4ad2e516c296c3851256fa3200ad53ece
JK-mern <Jkmerndev@gmail.com>
2024-01-29 15:31:10 +0530

    created getAll Problems function inorder to get all problems that can be shown in problem sections for users

--- server/controllers/problem.controller.js
+++ server/controllers/problem.controller.js
@@ -3,7 +3,7 @@ import { errorHandler } from "../utils/error.js";
 
 export const addProblems = async (req, res, next) => {
   const { title } = req.body;
-  const checkExist =await Problem.findOne({ title });
+  const checkExist = await Problem.findOne({ title });
   if (checkExist)
     return next(errorHandler(403, "Problem with same title already exist"));
 
@@ -13,5 +13,23 @@ export const addProblems = async (req, res, next) => {
     res.status(200).json(newProblem);
   } catch (error) {
     next(error);
+  }
+};
+
+export const getAllProblems = async (req, res, next) => {
+  try {
+    const skipValue = parseInt(req.query.skip) || 0;
+    console.log(skipValue);
+    const problems = await Problem.find()
+      .skip(skipValue)
+      .sort({ createdAt: 1 })
+      .limit(10);
+    const problem = problems.map((prob) => ({
+      title: prob.title,
+      difficulty: prob.difficulty,
+    }));
+    res.json(problem);
+  } catch (error) {
+    next(error)
   }
 };
--- server/routes/problem.route.js
+++ server/routes/problem.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { addProblems } from "../controllers/problem.controller.js";
+import { addProblems,getAllProblems } from "../controllers/problem.controller.js";
 
 const router = express.Router()
 
 router.post('/addproblems', addProblems)
+router.get('/all',getAllProblems)
 
 export default router
