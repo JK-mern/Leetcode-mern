a139ed6398f3f1950a9a5053fa27cf5ec69142df
JK-mern <Jkmerndev@gmail.com>
2024-01-31 00:13:22 +0530

    Added route to redirect to each problem

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Problems from "./Pages/Problems";
 import About from "./Pages/About";
 import PrivateRoute from "./Components/PrivateRoute";
 import AddProblems from "./Pages/AddProblems";
+import Problem from "./Pages/Problem";
 
 function App() {
   return (
@@ -17,12 +18,13 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/problems" element={<Problems />} />
-          <Route path="/addproblems" element = {< AddProblems/>} />
+          <Route path="/addproblems" element={<AddProblems />} />
           <Route path="/about" element={<About />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
+            <Route path="/problems/:title" element={<Problem />} />
           </Route>
         </Routes>
       </BrowserRouter>
