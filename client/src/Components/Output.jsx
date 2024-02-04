e67d9e473e2168c7b3c8dfd8d7138e11e47e46c1
JK-mern <Jkmerndev@gmail.com>
2024-02-05 01:17:02 +0530

    created a component to show output after code compilation

---
+++ client/src/Components/Output.jsx
@@ -0,0 +1,11 @@
+function Output({output}) {
+
+
+  return (
+    <div>
+      <div className="h-40 w-full bg-slate-950 text-white rounded-lg p-3 text-sm">{output}</div>
+    </div>
+  );
+}
+
+export default Output;
