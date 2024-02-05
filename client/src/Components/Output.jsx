6a89a5aa99e2e60891bbcdaa5531fe3fd5988fee
JK-mern <Jkmerndev@gmail.com>
2024-02-06 01:43:02 +0530

    code compilation completed

--- client/src/Components/Output.jsx
+++ client/src/Components/Output.jsx
@@ -1,9 +1,10 @@
-function Output({output}) {
-
-
+function Output({ output }) {
+  console.log(output);
   return (
     <div>
-      <div className="h-40 w-full bg-slate-950 text-white rounded-lg p-3 text-sm">{output}</div>
+      <div className="h-40 w-full bg-slate-950 text-white rounded-lg p-3 text-sm">
+        {output}
+      </div>
     </div>
   );
 }
--- client/src/Pages/Problem.jsx
+++ client/src/Pages/Problem.jsx
@@ -7,6 +7,8 @@ import Editor, { useMonaco } from "@monaco-editor/react";
 import nightOwl from "monaco-themes/themes/Night Owl.json";
 import { languageOptions } from "../constants/languageOption.js";
 import Output from "../Components/Output.jsx";
+import Toast from "../Components/Toast.jsx";
+import { toast } from "react-toastify";
 
 function Problem() {
   const monaco = useMonaco();
@@ -22,15 +24,77 @@ function Problem() {
     setCode(value);
   };
 
+  const handleCompile = async () => {
+    const formData = {
+      language_id: language.id,
+      source_code: btoa(code),
+    };
+    try {
+      const res = await axios.post(
+        ` ${import.meta.env.VITE_RAPID_API_URL}`,
+        formData,
+        {
+          params: {
+            base64_encoded: "true",
+            fields: "*",
+          },
+          headers: {
+            "content-type": "application/json",
+            "Content-Type": "application/json",
+            "X-RapidAPI-Host": import.meta.env.VITE_RAPID_API_HOST,
+            "X-RapidAPI-Key": import.meta.env.VITE_RAPID_API_KEY,
+          },
+        }
+      );
+      const token = res.data.token;
+      checkStatus(token);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const checkStatus = async (token) => {
+    try {
+      const res = await axios.get(
+        `${import.meta.env.VITE_RAPID_API_URL}/${token}`,
+        {
+          params: {
+            base64_encoded: "true",
+            fields: "*",
+          },
+          headers: {
+            "content-type": "application/json",
+            "Content-Type": "application/json",
+            "X-RapidAPI-Host": import.meta.env.VITE_RAPID_API_HOST,
+            "X-RapidAPI-Key": import.meta.env.VITE_RAPID_API_KEY,
+          },
+        }
+      );
+      let statusId = res.data.status?.id;
+      if (statusId === 1 || statusId === 2) {
+        // still processing
+        setTimeout(() => {
+          checkStatus(token);
+        }, 2000);
+        return;
+      } else {
+        toast.success(`Compiled Successfully!`);
+        const data = atob(res.data.stdout);
+        setOutput(data);
+        return;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const handleLanguage = (e) => {
     const selectedId = parseInt(e.target.value);
     const selectedLanguage = languageOptions.find(
       (language) => language.id === selectedId
     );
-    setLanguage(selectedLanguage)
+    setLanguage(selectedLanguage);
   };
-
-
 
   useEffect(() => {
     if (monaco) {
@@ -53,11 +117,12 @@ function Problem() {
 
   return (
     <main className="max-w-6xl mx-auto my-10 ">
+      <Toast />
       {loading ? (
         <Loader />
       ) : (
         <div className="flex flex-col md:flex-row">
-          <div className="w-6/12 p-3">
+          <div className=" md:w-6/12 p-3">
             <h1 className="text-lg  text-slate-50 font-bold">
               {currentProblem.title}
             </h1>
@@ -108,8 +173,8 @@ function Problem() {
               ))}
             </div>
           </div>
-          <div className="w-6/12 ">
-            <div className="flex justify-between p-3">
+          <div className=" md:w-6/12 ">
+            <div className="flex  flex-col md:flex-row gap-5 md:gap-0 md:justify-between p-3">
               <div className="flex  items-center">
                 <label className="text-gray-400 font-bold">Language : </label>
                 <select
@@ -123,10 +188,24 @@ function Problem() {
                   ))}
                 </select>
               </div>
-              <button className="btn  glass">Submit</button>
+              <div className=" md:ml-4  ">
+                <button
+                  className="btn  bg-red-900 w-full"
+                  onClick={handleCompile}
+                >
+                  Run
+                </button>
+              </div>
+              <div>
+                <button className="btn  bg-green-600 w-full">Submit</button>
+              </div>
             </div>
-            <div className="p-3 h-3/5">
-              <Editor theme="nightOwl" onChange={handleChange} language={language.value} />
+            <div className="p-3 h-72  md:h-3/5">
+              <Editor
+                theme="nightOwl"
+                onChange={handleChange}
+                language={language.value}
+              />
               <div className="mt-5">
                 <h3 className="font-semibold text-lg my-3 pl-3">Output</h3>
                 <Output output={output} />
