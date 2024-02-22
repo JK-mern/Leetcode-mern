3ccb8545f971bcb515a49ab2dc3bee0478b3909f
JK-mern <Jkmerndev@gmail.com>
2024-02-22 22:12:11 +0530

    designed the not found component

--- client/src/Components/NotFound.jsx
+++ client/src/Components/NotFound.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function NotFound() {
   return (
-    <div>
-      <h3>Page not Found</h3>
-    </div>
-  )
+
+      <div className="hero">
+        <div className="text-center hero-content">
+          <div className="max-w-6xl flex flex-col items-center justify-center h-screen">
+            <h1 className="text-6xl font-bold mb-8">Oops!</h1>
+            <p className="text-3xl ">404 - Page Not Found!</p>
+            <Link className="btn btn-primary btn-lg mt-10" to="/">
+            
+              Back To Home
+            </Link>
+          </div>
+        </div>
+      </div>
+   
+  );
 }
 
-export default NotFound
+export default NotFound;
