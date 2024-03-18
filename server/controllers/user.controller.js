81aec7e12ac149d8d51a9c4d54d46957a5758896
jkmerndev <jkmerndev@gmail.com>
2024-03-18 19:08:22 +0530

    created a backend function in order to update the user Profile data

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,17 +1,45 @@
-import User from "../models/user.model.js"
-import { errorHandler } from "../utils/error.js"
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcrypt from "bcryptjs";
 
-export const deleteUser = async(req,res,next) =>{
-    if(req.user.id !== req.params.id)
-        return next(errorHandler(401, "You can only update your own account") )
-    else
-    {
-        try {
-            await User.findOneAndDelete(req.params.id)
-            res.clearCookie('accessToken')
-            res.status(200).json("User deleted Successfully")
-        } catch (error) {
-            next(error)            
-        }
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only update your own account"));
+  else {
+    try {
+      await User.findOneAndDelete(req.params.id);
+      res.clearCookie("accessToken");
+      res.status(200).json("User deleted Successfully");
+    } catch (error) {
+      next(error);
     }
-}
+  }
+};
+
+export const updateUser = async (req, res, next) => {
+  try {
+
+    console.log(req.user.id,req.params.id)
+
+    if (req.user.id !== req.params.id)
+      return next(errorHandler(401, "You can only Update your Profile!"));
+
+    if (req.body.password)
+      req.body.password = bcrypt.hashSync(req.body.password, 10);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.user.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.username,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+    updatedUser.password = undefined;
+    res.status(200).json(updatedUser);
+  } catch (error) {}
+};
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { verifyUser } from '../utils/verify.js'
-import { deleteUser } from '../controllers/user.controller.js'
+import { deleteUser, updateUser } from '../controllers/user.controller.js'
 
 const router = express.Router()
 
 router.delete('/delete/:id', verifyUser, deleteUser)
+router.put('/update/:id',verifyUser, updateUser)
 
 export default router
