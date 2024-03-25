46fd40a345fc4dc1a74466358192a95218d896e4
jkmerndev <jkmerndev@gmail.com>
2024-03-25 22:53:06 +0530

    updated problem library with access to Roadmap

--- client/src/Pages/ProblemLibrary.jsx
+++ client/src/Pages/ProblemLibrary.jsx
@@ -18,7 +18,7 @@ function ProblemLibrary() {
       setQuestions(data);
     };
     getQuestions();
-    
+
     setTimeout(() => {
       setLoading(false);
     }, 400);
@@ -36,61 +36,72 @@ function ProblemLibrary() {
       {loading ? (
         <Loader />
       ) : (
-        <div className="overflow-x-auto">
-          <table className="table ">
-            {/* head */}
-            <thead>
-              <tr>
-                <th>No.</th>
-                <th>Title</th>
-                <th>Difficulty Level</th>
-              </tr>
-            </thead>
-            <tbody>
-              {questions.length >= 1 &&
-                questions.map((question, index) => (
-                  <tr key={index}>
-                    <th>{skipValue + index + 1}</th>
-
-                    <td className="cursor-pointer hover:underline">
-                      <Link to={`/problems/${question.title}`}>
-                        {question.title}
-                      </Link>
-                    </td>
-                    <td
-                      className={
-                        question.difficulty === "Easy"
-                          ? "text-green-600"
-                          : question.difficulty === "Medium"
-                          ? "text-amber-500"
-                          : question.difficulty === "Hard"
-                          ? "text-red-500"
-                          : ""
-                      }
-                    >
-                      {question.difficulty}
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
-          <div className="flex flex-row my-6 justify-center gap-4  ">
-            <button
-              className="btn btn-active btn-ghost"
-              disabled={skipValue === 0}
-              onClick={handlePrev}
-            >
-              prev
-            </button>
-            <button
-              className="btn btn-active btn-ghost "
-              disabled={questions.length < 10}
-              onClick={handleNext}
-            >
-              next
-            </button>
+        <>
+          <div className="my-5 mb-10 p-5  border border-zinc-600 rounded-3xl flex justify-between items-center">
+            <p className="tracking-wider font-serif">
+              Would you like to take a look at the pattern based learning for
+              better understanding?{" "}
+            </p>
+            <Link to="/Roadmap" >  <button className=" btn  btn-primary">Click here</button></Link>
+          
           </div>
-        </div>
+
+          <div className="overflow-x-auto">
+            <table className="table ">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th>No.</th>
+                  <th>Title</th>
+                  <th>Difficulty Level</th>
+                </tr>
+              </thead>
+              <tbody>
+                {questions.length >= 1 &&
+                  questions.map((question, index) => (
+                    <tr key={index}>
+                      <th>{skipValue + index + 1}</th>
+
+                      <td className="cursor-pointer hover:underline">
+                        <Link to={`/problems/${question.title}`}>
+                          {question.title}
+                        </Link>
+                      </td>
+                      <td
+                        className={
+                          question.difficulty === "Easy"
+                            ? "text-green-600"
+                            : question.difficulty === "Medium"
+                            ? "text-amber-500"
+                            : question.difficulty === "Hard"
+                            ? "text-red-500"
+                            : ""
+                        }
+                      >
+                        {question.difficulty}
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+            <div className="flex flex-row my-6 justify-center gap-4  ">
+              <button
+                className="btn btn-active btn-ghost"
+                disabled={skipValue === 0}
+                onClick={handlePrev}
+              >
+                prev
+              </button>
+              <button
+                className="btn btn-active btn-ghost "
+                disabled={questions.length < 10}
+                onClick={handleNext}
+              >
+                next
+              </button>
+            </div>
+          </div>
+        </>
       )}
     </main>
   );
