67b5f19e16b9678159a12f2b954d770c3a352cea
jkmerndev <jkmerndev@gmail.com>
2024-03-28 23:40:42 +0530

    created the componenent for roadmap

--- client/src/Pages/Roadmap.jsx
+++ client/src/Pages/Roadmap.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Roadmap() {
   return (
-    <div>Roadmap</div>
-  )
+    <div className="max-w-7xl mx-auto   my-5 p-3 ">
+      <h1 className="text-left ml-2 text-xl font-bold">Recommended Roadmap</h1>
+      <div className="flex justify-between  items-center py-3 my-8 rounded-lg bg-neutral-800 w-4/12 ">
+        <div>
+          <p className="text-white ml-6">Array</p>
+        </div>
+        <button className=" mx-5 btn btn-success">&#8964;</button>
+      </div>
+    </div>
+  );
 }
 
-export default Roadmap
+export default Roadmap;
