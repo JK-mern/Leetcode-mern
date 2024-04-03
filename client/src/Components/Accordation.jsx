39f2264f4f60ea8d64da167595119a9e18112cf5
JK-mern <Jkmerndev@gmail.com>
2024-04-04 01:36:28 +0530

    basic version of accordation

--- client/src/Components/Accordation.jsx
+++ client/src/Components/Accordation.jsx
@@ -1,60 +1,67 @@
 import React, { useState } from "react";
 
-function Accordation() {
-    const [isOpen, setIsOpen] = useState(false);
+function Accordation(props) {
+  const [isOpen, setIsOpen] = useState(false);
+
 
   const toggleAccordion = () => {
     setIsOpen(!isOpen);
   };
+
+ 
+
+
   return (
+    <div className="  w-full lg:w-3/12 border border-teal-700 rounded mt-8 ">
+      <div
+        className="flex justify-between items-center p-4 cursor-pointer"
+        id={props.id}
     
-      <div className=" w-4/12 border border-teal-700 rounded mt-8 ">
-        <div
-          className="flex justify-between items-center p-4 cursor-pointer"
-          onClick={toggleAccordion}
+        onClick = {toggleAccordion}
+        
+      >
+        <h2 className="text-lg font-semibold">{props.title}</h2>
+        <svg
+          className={`w-6 h-6 ${isOpen ? "transform rotate-180" : ""}`}
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
         >
-          <h2 className="text-lg font-semibold">Arrays</h2>
-          <svg
-            className={`w-6 h-6 ${isOpen ? "transform rotate-180" : ""}`}
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M19 9l-7 7-7-7"
-            />
-          </svg>
-        </div>
-        {isOpen && (
-          <div className=" flex flex-col justify-center text-center w-full">
-            <ul className="">
-              <hr />
-              <li className="mt-4">
-                <a href="#" className=" font-mono">
-                  Link 1
-                </a>
-                <hr />
-              </li>
-              <li className="mt-4">
-                <a href="#" className=" font-mono ">
-                  Link 2
-                </a>
-                <hr />
-              </li>
-              <li className="mt-4">
-                <a href="#" className=" font-mono ">
-                  Link 3
-                </a>
-                <hr />
-              </li>
-            </ul>
-          </div>
-        )}
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M19 9l-7 7-7-7"
+          />
+        </svg>
       </div>
-    
+
+      {isOpen && (
+        <div className=" flex flex-col justify-center text-center w-full">
+          <ul className="">
+            <hr />
+            <li className="mt-4">
+              <a href="#" className=" font-mono">
+                Link 1
+              </a>
+              <hr />
+            </li>
+            <li className="mt-4">
+              <a href="#" className=" font-mono ">
+                Link 2
+              </a>
+              <hr />
+            </li>
+            <li className="mt-4">
+              <a href="#" className=" font-mono ">
+                Link 3
+              </a>
+              <hr />
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
   );
 }
 
--- client/src/Pages/Roadmap.jsx
+++ client/src/Pages/Roadmap.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import Accordation from "../Components/Accordation";
 
 function Roadmap() {
   return (
     <div className="max-w-7xl mx-auto   my-5 p-3 ">
-      <h1 className="text-left ml-2 text-xl font-bold">Recommended Roadmap</h1>
-      <div className="flex justify-between  items-center py-3 my-8 rounded-lg bg-neutral-800 w-4/12 ">
-        <div>
-          <p className="text-white ml-6">Array</p>
-        </div>
-        <button className=" mx-5 btn btn-success">&#8964;</button>
+      <h1 className="text-left ml-2 text-xl font-bold">
+        Recommended Roadmap :{" "}
+      </h1> 
+      <div className="flex    lg:gap-20 justify-start    flex-wrap">
+        <Accordation  id={1} title= "Arrays & Hasing"/>
+
+        <Accordation id={2}   title= "Two Pointers"/>
+        <Accordation  id={3}  title= "Stack "/>
+        <Accordation  id={4}  title= "Sliding Window"/>
+        <Accordation  id={5}  title= "Linked List"/>
+        <Accordation  id={6}  title= "Binary Search "/>
+        <Accordation  id={6}  title= "Trees "/>
+        <Accordation  id={6}  title= "Heap/Priority Queue "/>
+        <Accordation  id={6}  title= "Back Tracking "/>
       </div>
     </div>
   );
 }
 
 export default Roadmap;
+
+// <div className="max-w-7xl mx-auto   my-5 p-3 ">
+//       <h1 className="text-left ml-2 text-xl font-bold">Recommended Roadmap</h1>
+//       <div className="flex justify-between  items-center py-3 my-8 rounded-lg bg-neutral-800 w-4/12 ">
+//         <div>
+//           <p className="text-white ml-6">Array</p>
+//         </div>
+//         <button className=" mx-5 btn btn-success">&#8964;</button>
+//       </div>
+//     </div>
