55e35360aec271aea88694a72b18ff79e5249936
JK-mern <Jkmerndev@gmail.com>
2024-04-15 20:45:06 +0530

    changed vite config for front end hosting

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{target:'http://localhost:3001',
-      secure:false,
-    }
-    }
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://backend-iota-ten-73.vercel.app/",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   plugins: [react()],
-})
+});
